af71fe606a82a0ca67616f2abf06a540c7f1ca6e
bfk <bfker@qq.com>
2020-06-23 00:19:16 +0800

    project2-2

--- Project2/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('DBHOST', 'localhost');
-define('DBNAME', 'project2');
-define('DBUSER', 'root');
-define('DBPASS', 'Betty123456815');
-define('DBCONNSTRING','mysql:host=localhost;dbname=project2');
-?>
--- Project2/src/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['Username']);
-header("location:http://localhost/project2/src/login.php");
-?>
